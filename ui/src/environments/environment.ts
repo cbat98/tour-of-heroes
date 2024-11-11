dfaee720144171066a7aad3e6a727d8cbcda37c9
Charlie B <charlie@charliebatten.co.uk>
2024-11-11 20:38:42 +0000

    Add hosts for nixlt

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiHost: "http://cbarchvm01",
-  apiPort: "5152",
+  apiHost: "http://charlie-nixlt",
+  apiPort: "8080",
   apiRoute: "api/v1"
 };
