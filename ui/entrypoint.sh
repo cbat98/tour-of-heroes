1779600b2b4df792eede063b34bdfd39e205646e
Charlie Batten <charlie.batten@microlise.com>
2024-11-14 09:29:27 +0000

    Add ui runtime config to UI

--- ui/entrypoint.sh
+++ ui/entrypoint.sh
@@ -2,4 +2,10 @@
 
 set -e
 
-echo "Hello, World!"
+wwwroot_dir=/usr/share/nginx/html
+
+sed -i "s|#{apiHost}#|$API_HOST|g" $wwwroot_dir/assets/config.json
+sed -i "s|#{apiRoute}#|$API_ROUTE|g" $wwwroot_dir/assets/config.json
+
+echo "Config loaded:"
+cat $wwwroot_dir/assets/config.json
