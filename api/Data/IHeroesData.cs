f30d71bd5dadf901e10a92523013d32ef14c3859
Charlie Batten <charlie.batten@microlise.com>
2024-11-14 11:18:17 +0000

    Add interface for heroes data adapter

---
+++ api/Data/IHeroesData.cs
@@ -0,0 +1,12 @@
+using TourOfHeroes.API.Models;
+
+namespace TourOfHeroes.API.Data;
+
+public interface IHeroesData
+{
+    public IList<Hero> GetHeroes(string name);
+    public Hero? GetHero(int id);
+    public Hero? UpdateHero(Hero hero, string name);
+    public Hero AddHero(NewHeroDto newHero);
+    public bool DeleteHero(int id);
+}
