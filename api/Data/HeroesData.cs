7c702e2355e491532a8e80b56aeb988d5bc08e28
Charlie Batten <charlie.batten@microlise.com>
2024-11-14 13:30:21 +0000

    Convert addhero to sql

--- api/Data/HeroesData.cs
+++ api/Data/HeroesData.cs
@@ -80,10 +80,6 @@ public class HeroesData : IHeroesData
 
     public Hero? UpdateHero(Hero hero, string name)
     {
-        /*var success = hero.SetName(name);*/
-        /**/
-        /*return (success) ? hero : null;*/
-
         var query =
             @"
                 UPDATE tbl_heroes
@@ -99,19 +95,43 @@ public class HeroesData : IHeroesData
         using var connection = new SqliteConnection(_dataConfig.ConnectionString);
         connection.Open();
 
-        var result = HeroesDbHelper.CreateCommand(connection, query, parameters).ExecuteNonQuery();
+        var rowsChanged = HeroesDbHelper.CreateCommand(connection, query, parameters).ExecuteNonQuery();
 
-        return result > 0 ? new Hero(hero.Id, name) : null;
+        return rowsChanged > 0 ? new Hero(hero.Id, name) : null;
     }
 
     public Hero AddHero(NewHeroDto newHero)
     {
-        var id = _heroes.Max(h => h.Id) + 1;
-        var hero = new Hero(id, newHero.Name);
+        var query =
+            @"
+                INSERT INTO tbl_heroes (name)
+                VALUES ($name)
+            ";
 
-        _heroes.Add(hero);
+        var parameters = new Dictionary<string, object> {
+            { "name", newHero.Name }
+        };
 
-        return hero;
+        using var connection = new SqliteConnection(_dataConfig.ConnectionString);
+        connection.Open();
+
+        HeroesDbHelper.CreateCommand(connection, query, parameters).ExecuteReader();
+
+        query =
+            @"
+                SELECT id, name
+                FROM tbl_heroes
+                WHERE id = last_insert_rowid()
+            ";
+
+        var reader = HeroesDbHelper.CreateCommand(connection, query).ExecuteReader();
+
+        reader.Read();
+
+        var heroId = reader.GetInt32(0);
+        var heroName = reader.GetString(1);
+
+        return new Hero(heroId, heroName);
     }
 
     public bool DeleteHero(int id)
