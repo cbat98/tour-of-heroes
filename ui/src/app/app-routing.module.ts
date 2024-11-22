8c93b7f35f0966c2f7cd81ad601fc4ed38d63920
Charlie Batten <charlie.batten@microlise.com>
2024-11-22 14:30:39 +0000

    Change routing to details

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'detail/:id', component: HeroDetailComponent },
+  { path: 'heroes/:id', component: HeroDetailComponent },
 ];
 
 @NgModule({
