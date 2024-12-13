c1403b30a13e9e08bd2e1908b23c0866b71f7608
Charlie Batten <charlie.batten@microlise.com>
2024-12-13 15:08:55 +0000

    Lowercase name match for conflict

--- api/Services/HeroesService.cs
+++ api/Services/HeroesService.cs
@@ -14,7 +14,7 @@ public class HeroesService : IHeroesService
 
     public async Task<Hero?> AddHeroAsync(NewHeroDto newHeroDto)
     {
-        var existingHero = (await _heroesData.GetHeroesAsync("")).FirstOrDefault(h => h.Name == newHeroDto.Name);
+        var existingHero = (await _heroesData.GetHeroesAsync("")).FirstOrDefault(h => h.Name.ToLower() == newHeroDto.Name.ToLower());
         if (existingHero is not null)
         {
             return await Task.FromResult<Hero?>(null);
@@ -40,7 +40,7 @@ public class HeroesService : IHeroesService
 
     public async Task<Hero?> UpdateHeroAsync(Hero hero, string name)
     {
-        var existingHero = (await _heroesData.GetHeroesAsync("")).FirstOrDefault(h => h.Name == name && h.Id != hero.Id);
+        var existingHero = (await _heroesData.GetHeroesAsync("")).FirstOrDefault(h => h.Name.ToLower() == name.ToLower() && h.Id != hero.Id);
         if (existingHero is not null)
         {
             return await Task.FromResult<Hero?>(null);
